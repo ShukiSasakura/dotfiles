afc5ebe9447d4e2b63cdff144a7aaf44b3296f24
ShukiSasakura <pbje8wfi@s.okayama-u.ac.jp>
2024-08-05 12:10:56 +0900

    Add neo-tree

--- nvim/init.lua
+++ nvim/init.lua
@@ -141,6 +141,17 @@ require("lazy").setup({
             'nvim-tree/nvim-web-devicons', -- optional
         }
     },
+    -- filesystem をツリー表示
+    {
+        "nvim-neo-tree/neo-tree.nvim",
+        branch = "v3.x",
+        dependencies = {
+            "nvim-lua/plenary.nvim",
+            "nvim-tree/nvim-web-devicons", -- not strictly required, but recommended
+            "MunifTanjim/nui.nvim",
+            -- "3rd/image.nvim", -- Optional image support in preview window: See `# Preview Mode` for more information
+        }
+    },
     -- colorscheme
     { "catppuccin/nvim",lazy = false, name = "catppuccin", priority = 1000 },
     --LSP 関連
