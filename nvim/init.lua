a22e8b12b307c56460b0c460fe0c9b26aa674dab
ShukiSasakura <pbje8wfi@s.okayama-u.ac.jp>
2024-09-13 14:53:41 +0900

    Add noice.nvim

--- nvim/init.lua
+++ nvim/init.lua
@@ -159,6 +159,22 @@ require("lazy").setup({
             -- "3rd/image.nvim", -- Optional image support in preview window: See `# Preview Mode` for more information
         }
     },
+    -- コマンドラインを中央に，メッセージを右上に持ってくる
+    {
+        "folke/noice.nvim",
+        event = "VeryLazy",
+        opts = {
+            -- add any options here
+        },
+        dependencies = {
+            -- if you lazy-load any plugin below, make sure to add proper `module="..."` entries
+            "MunifTanjim/nui.nvim",
+            -- OPTIONAL:
+            --   `nvim-notify` is only needed, if you want to use the notification view.
+            --   If not available, we use `mini` as the fallback
+            "rcarriga/nvim-notify",
+        }
+    },
     -- colorscheme
     { "catppuccin/nvim",lazy = false, name = "catppuccin", priority = 1000 },
     --LSP 関連
@@ -266,6 +282,26 @@ require("ibl").setup()
 
 -- triptych.nvim のセットアップ
 require 'triptych'.setup()
+
+-- noice.nvim のセットアップ
+require("noice").setup({
+  lsp = {
+    -- override markdown rendering so that **cmp** and other plugins use **Treesitter**
+    override = {
+      ["vim.lsp.util.convert_input_to_markdown_lines"] = true,
+      ["vim.lsp.util.stylize_markdown"] = true,
+      ["cmp.entry.get_documentation"] = true, -- requires hrsh7th/nvim-cmp
+    },
+  },
+  -- you can enable a preset for easier configuration
+  presets = {
+    bottom_search = true, -- use a classic bottom cmdline for search
+    command_palette = true, -- position the cmdline and popupmenu together
+    long_message_to_split = true, -- long messages will be sent to a split
+    inc_rename = false, -- enables an input dialog for inc-rename.nvim
+    lsp_doc_border = false, -- add a border to hover docs and signature help
+  },
+})
 
 --catppuccin(colorscheme) のセットアップ
 require("catppuccin").setup({
